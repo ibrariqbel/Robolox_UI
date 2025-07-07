829100e768e8d3805264ac5c844c29bb4cb49b71
ibrariqbel <ibrar.iqbel@gmail.com>
2025-07-07 15:23:46 +0500

    Ui

--- robotrace/src/ShareComponents/Card.jsx
+++ robotrace/src/ShareComponents/Card.jsx
@@ -1,30 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react'
+import { context } from '../Context/Store'
 
-const Card = ({ title, description, imageUrl, buttonText, onButtonClick, footerText, children }) => {
+const Card = () => {
+  const {username} = useContext(context);
+ 
   return (
-    <div className="card h-100 shadow-sm border-0"> {/* h-100 for equal height cards in flex container */}
-      {imageUrl && (
-        <img src={imageUrl} className="card-img-top" alt={title || "Card image"} />
-      )}
-      <div className="card-body d-flex flex-column"> {/* Use flexbox for spacing */}
-        {children} {/* This is where the service icon will be rendered */}
-        {title && <h5 className="card-title text-primary mt-2 mb-2">{title}</h5>}
-        {description && <p className="card-text text-secondary mb-3 flex-grow-1">{description}</p>} {/* flex-grow-1 makes description take available space */}
-        {onButtonClick && buttonText && (
-          <div className="mt-auto"> {/* Pushes button to the bottom */}
-            <button onClick={onButtonClick} className="btn btn-outline-primary btn-sm w-100">
-              {buttonText}
-            </button>
-          </div>
-        )}
-      </div>
-      {footerText && (
-        <div className="card-footer bg-light-subtle text-muted small py-2 px-3">
-          {footerText}
-        </div>
-      )}
+    <div>
+        <h1>
+          the name of user is {username}
+        </h1>
     </div>
-  );
-};
+  )
+}
 
-export default Card;
+export default Card
--- robotrace/src/index.js
+++ robotrace/src/index.js
@@ -1,8 +1,5 @@
 import React from "react";
 import reactDom from "react-dom/client";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'animate.css/animate.min.css'; // For animations
-// No need for 'bootstrap-icons/font/bootstrap-icons.css' if using React Icons
 
 import { BrowserRouter } from "react-router-dom";
 import Store from "./Context/Store";
